2cc319be3007b91b86f9171ba32e8ada279ed8e7
wpeterashworth <wingsaseagles12@gmail.com>
2024-02-19 16:11:25 -0800

    Created Favorite Games separate decision page

--- entertainment_choices/scripts/game_filter.js
+++ entertainment_choices/scripts/game_filter.js
@@ -1,51 +1,87 @@
-function filterGames() {
-//     let filter = document.querySelector("#filter").value;
-//     let allGames = {
-//         singlePlayerGame1: document.querySelector(".singleplayer-game-1").textContent,
-//         singlePlayerGame2: document.querySelector(".singleplayer-game-2").textContent,
-//         // singlePlayerGame3: document.querySelector(".singleplayer-game-3").textContent,
-//         // singlePlayerGame4: document.querySelector(".singleplayer-game-4").textContent,
-//         // singlePlayerGame5: document.querySelector(".singleplayer-game-5").textContent,
-//         // singlePlayerGame6: document.querySelector(".singleplayer-game-6").textContent,
-//         // singlePlayerGame7: document.querySelector(".singleplayer-game-7").textContent,
-//         // singlePlayerGame8: document.querySelector(".singleplayer-game-8").textContent,
-//         // singlePlayerGame9: document.querySelector(".singleplayer-game-9").textContent,
-//         // singlePlayerGame10: document.querySelector(".singleplayer-game-10").textContent,
-//         // singlePlayerGame11: document.querySelector(".singleplayer-game-11").textContent,
-//         // singlePlayerGame12: document.querySelector(".singleplayer-game-12").textContent,
+let Games = []
+let game1 = document.querySelector(".game1").textContent
+let game2 = document.querySelector(".game2").textContent
+let game3 = document.querySelector(".game3").textContent
+let game4 = document.querySelector(".game4").textContent
+let game5 = document.querySelector(".game5").textContent
+let game6 = document.querySelector(".game6").textContent
+let game7 = document.querySelector(".game7").textContent
+let game8 = document.querySelector(".game8").textContent
+let game9 = document.querySelector(".game9").textContent
+let game10 = document.querySelector(".game10").textContent
+let game11 = document.querySelector(".game11").textContent
+let game12 = document.querySelector(".game12").textContent
+let game13 = document.querySelector(".game13").textContent
+let game14 = document.querySelector(".game14").textContent
+let game15 = document.querySelector(".game15").textContent
+let game16 = document.querySelector(".game16").textContent
+let game17 = document.querySelector(".game17").textContent
+let game18 = document.querySelector(".game18").textContent
+let game19 = document.querySelector(".game19").textContent
+let game20 = document.querySelector(".game20").textContent
+let game21 = document.querySelector(".game21").textContent
+let game22 = document.querySelector(".game22").textContent
+let game23 = document.querySelector(".game23").textContent
+let game24 = document.querySelector(".game24").textContent
+let game25 = document.querySelector(".game25").textContent
+let game26 = document.querySelector(".game26").textContent
+let game27 = document.querySelector(".game27").textContent
+let game28 = document.querySelector(".game28").textContent
+let game29 = document.querySelector(".game29").textContent
+let game30 = document.querySelector(".game30").textContent
+let game31 = document.querySelector(".game31").textContent
+let game32 = document.querySelector(".game32").textContent
+let game33 = document.querySelector(".game33").textContent
+let game34 = document.querySelector(".game34").textContent
+let game35 = document.querySelector(".game35").textContent
+let game36 = document.querySelector(".game36").textContent
+let game37 = document.querySelector(".game37").textContent
+let game38 = document.querySelector(".game38").textContent
+let game39 = document.querySelector(".game39").textContent
+let game40 = document.querySelector(".game40").textContent
+let game41 = document.querySelector(".game41").textContent
+let game42 = document.querySelector(".game42").textContent
+let game43 = document.querySelector(".game43").textContent
+let game44 = document.querySelector(".game44").textContent
+let game45 = document.querySelector(".game45").textContent
+let game46 = document.querySelector(".game46").textContent
+let game47 = document.querySelector(".game47").textContent
+let game48 = document.querySelector(".game48").textContent
+let game49 = document.querySelector(".game49").textContent
+let game50 = document.querySelector(".game50").textContent
+let game51 = document.querySelector(".game51").textContent
+let game52 = document.querySelector(".game52").textContent
+let game53 = document.querySelector(".game53").textContent
+let game54 = document.querySelector(".game54").textContent
+let game55 = document.querySelector(".game55").textContent
+let game56 = document.querySelector(".game56").textContent
+let game57 = document.querySelector(".game57").textContent
+let game58 = document.querySelector(".game58").textContent
+let game59 = document.querySelector(".game59").textContent
+let game60 = document.querySelector(".game60").textContent
+Games.push(game1, game2, game3, game4, game5, game6, game7, game8, game9, game10,
+    game11, game12, game13, game14, game15, game16, game17, game18, game19, game20, game21,
+    game22, game23, game24, game25, game26, game27, game28, game29, 
+    game30, game31, game32, game33, game34, game35, game36, game37, game38, game39,
+    game40, game41, game42, game43, game44, game45, game46, game47, game48, game49,
+    game50, game51, game52, game53, game54, game55, game56, game57, game58, game59, game60)
+console.log(Games)
+
+// function filterGames() {
+//     let gameCategory = document.querySelector("#filter").value 
+//     let gameCategory2 = document.querySelector(".game-columns").value
+//     let noSingleGame = gameCategory2 != "single"
+//     if (gameCategory === "single" && gameCategory2 === "single") {
+//         document.body.remove(noSingleGame)
 //     }
-//     let fiveStarGames = document.querySelectorAll(".five_stars");
-//     let uncommonGames = document.querySelectorAll(".uncommon");
-//     let multiPlayerGames = document.querySelectorAll(".multi");
-//     let allGames = document.querySelector(".game-columns").innerText;
-//     switch(filter) {
-//         case "single":
-//             let output = 
-//                 singlePlayerGames.filter(employee => employee.department == "IT");
-//                 for (let i = 0; i < output.length; i++) {
-//                     console.log(output[i].name)
-//         break
-//     }
-//     console.log(allGames);
-//     console.log(singlePlayerGames);
 // }
-let singlePlayerGames = {
-    singlePlayerGame1: document.querySelector(".singleplayer-game-1").textContent,
-    singlePlayerGame2: document.querySelector(".singleplayer-game-2").textContent,
-}
 
-let multiplayerGames = {
-    multiplayerGame: document.querySelector(".multiplayerGame").textContent
-}
-let allGames = [
-   {style: singlePlayerGames,
-    multiplayerGames }
-    ]
-let output = 
-    allGames.filter(game => game.style == `${singlePlayerGames}`);
-    for (let i = 0; i < output.length; i++) {
-        console.log(output[i].name)
-}
-}
-filterGames();
+// document.querySelector("#filter").addEventListener("change", () => {filterGames})
 
+// This Comment
+// has more than 
+// one line
+
+let person = {
+    "hello": "hello"
+}
