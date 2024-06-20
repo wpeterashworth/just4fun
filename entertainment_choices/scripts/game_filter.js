b248d69c20548afbb1c7e2f56f2ff03b00662914
wpeterashworth <wingsaseagles12@gmail.com>
2024-06-19 21:47:51 -0600

    Created array filter for game list

--- entertainment_choices/scripts/game_filter.js
+++ entertainment_choices/scripts/game_filter.js
@@ -1,87 +1,71 @@
-let Games = []
-let game1 = document.querySelector(".game1").textContent
-let game2 = document.querySelector(".game2").textContent
-let game3 = document.querySelector(".game3").textContent
-let game4 = document.querySelector(".game4").textContent
-let game5 = document.querySelector(".game5").textContent
-let game6 = document.querySelector(".game6").textContent
-let game7 = document.querySelector(".game7").textContent
-let game8 = document.querySelector(".game8").textContent
-let game9 = document.querySelector(".game9").textContent
-let game10 = document.querySelector(".game10").textContent
-let game11 = document.querySelector(".game11").textContent
-let game12 = document.querySelector(".game12").textContent
-let game13 = document.querySelector(".game13").textContent
-let game14 = document.querySelector(".game14").textContent
-let game15 = document.querySelector(".game15").textContent
-let game16 = document.querySelector(".game16").textContent
-let game17 = document.querySelector(".game17").textContent
-let game18 = document.querySelector(".game18").textContent
-let game19 = document.querySelector(".game19").textContent
-let game20 = document.querySelector(".game20").textContent
-let game21 = document.querySelector(".game21").textContent
-let game22 = document.querySelector(".game22").textContent
-let game23 = document.querySelector(".game23").textContent
-let game24 = document.querySelector(".game24").textContent
-let game25 = document.querySelector(".game25").textContent
-let game26 = document.querySelector(".game26").textContent
-let game27 = document.querySelector(".game27").textContent
-let game28 = document.querySelector(".game28").textContent
-let game29 = document.querySelector(".game29").textContent
-let game30 = document.querySelector(".game30").textContent
-let game31 = document.querySelector(".game31").textContent
-let game32 = document.querySelector(".game32").textContent
-let game33 = document.querySelector(".game33").textContent
-let game34 = document.querySelector(".game34").textContent
-let game35 = document.querySelector(".game35").textContent
-let game36 = document.querySelector(".game36").textContent
-let game37 = document.querySelector(".game37").textContent
-let game38 = document.querySelector(".game38").textContent
-let game39 = document.querySelector(".game39").textContent
-let game40 = document.querySelector(".game40").textContent
-let game41 = document.querySelector(".game41").textContent
-let game42 = document.querySelector(".game42").textContent
-let game43 = document.querySelector(".game43").textContent
-let game44 = document.querySelector(".game44").textContent
-let game45 = document.querySelector(".game45").textContent
-let game46 = document.querySelector(".game46").textContent
-let game47 = document.querySelector(".game47").textContent
-let game48 = document.querySelector(".game48").textContent
-let game49 = document.querySelector(".game49").textContent
-let game50 = document.querySelector(".game50").textContent
-let game51 = document.querySelector(".game51").textContent
-let game52 = document.querySelector(".game52").textContent
-let game53 = document.querySelector(".game53").textContent
-let game54 = document.querySelector(".game54").textContent
-let game55 = document.querySelector(".game55").textContent
-let game56 = document.querySelector(".game56").textContent
-let game57 = document.querySelector(".game57").textContent
-let game58 = document.querySelector(".game58").textContent
-let game59 = document.querySelector(".game59").textContent
-let game60 = document.querySelector(".game60").textContent
-Games.push(game1, game2, game3, game4, game5, game6, game7, game8, game9, game10,
-    game11, game12, game13, game14, game15, game16, game17, game18, game19, game20, game21,
-    game22, game23, game24, game25, game26, game27, game28, game29, 
-    game30, game31, game32, game33, game34, game35, game36, game37, game38, game39,
-    game40, game41, game42, game43, game44, game45, game46, game47, game48, game49,
-    game50, game51, game52, game53, game54, game55, game56, game57, game58, game59, game60)
-console.log(Games)
+// function filterGames(gamesArray) {
 
-// function filterGames() {
-//     let gameCategory = document.querySelector("#filter").value 
-//     let gameCategory2 = document.querySelector(".game-columns").value
-//     let noSingleGame = gameCategory2 != "single"
-//     if (gameCategory === "single" && gameCategory2 === "single") {
-//         document.body.remove(noSingleGame)
+//     let filter = document.querySelector("#filter").value
+//     switch (filter) {
+//         case "single":
+//             return gamesArray.filter(game => game.value = single)
 //     }
 // }
 
-// document.querySelector("#filter").addEventListener("change", () => {filterGames})
+// let gamesArray = []
 
-// This Comment
-// has more than 
-// one line
+// // Example usage:
+// let games = document.querySelector(".game-list")
 
-let person = {
-    "hello": "hello"
+// // Loop through all the images (elements)
+// for (let i = 0; i < games.length; i++) {
+//     // Push the images(elements) to the the arrays
+//     gamesArray.push(games[i]); // push the elements
+// }
+
+// filterGames()
+
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("column");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
 }
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+
